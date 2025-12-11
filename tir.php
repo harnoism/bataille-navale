8a0e4a6f75f91d28880693e136b9288bf3bcb196
harnoism <harnois.maevaa@gmail.com>
2025-12-11 09:39:40 +0100

    essaie

--- tir.php
+++ tir.php
@@ -36,12 +36,18 @@ if (isset($_POST["joueur2"])) {
 if (isset($_POST["tirer"])) {
     $coordX = $_POST["x"] ?? null;
     $coordY = $_POST["y"] ?? null;
+    $color = $case['checked'] == 1 ? 'blue' : 'grey';
+    if ($case['checked'] == 1 && $case['boat'] > 0) {
+        $color = 'red';
+    }   
+        
+        $id_grid = $case['id_grid'];
     
     $etat["dernierCoup"] = [
         "joueur" => $_SESSION["role"],
         "x" => $coordX,
         "y" => $coordY,
-        "touche" => $touche 
+        "touche" => $touche
     ];
     
     file_put_contents($fichier, json_encode($etat));
