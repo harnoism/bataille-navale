5306025f27e9c3e753a9cb1a331866569746e536
harnoism <harnois.maevaa@gmail.com>
2025-12-12 10:02:08 +0100

    commentaires sur sql.connect

--- sql-connect.php
+++ sql-connect.php
@@ -1,10 +1,10 @@
 <?php
 
-class SqlConnect {
+class SqlConnect { //connexion PDO à MySQL. //Les propriétés private sont accessibles seulement dans la classe.
   public object $db;
   private string $host;
   private string $port;
-  private string $dbname;
+  private string $dbname; 
   private string $password;
   private string $user;
 
@@ -15,18 +15,18 @@ class SqlConnect {
     $this->user = 'root';
     $this->password = 'root';
 
-    $this->db = new PDO(
+    $this->db = new PDO( //Création de l'objet PDO
       'mysql:host='.$this->host.';port='.$this->port.';dbname='.$this->dbname,
       $this->user,
-      $this->password
+      $this->password  // "this" devient l'objet PDO
     );
 
-    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $this->db->setAttribute(PDO::ATTR_PERSISTENT, false);
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Active les exceptions en cas d’erreur SQL.
+    $this->db->setAttribute(PDO::ATTR_PERSISTENT, false); //Désactive les connexions persistantes.
   }
 
   public function transformDataInDot($data) {
-    $dataFormated = [];
+    $dataFormated = []; //Crée un tableau vide qui contiendra les données formatées.
 
     foreach ($data as $key => $value) {
       $dataFormated[':' . $key] = $value;
